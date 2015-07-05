4750665897a1fd8c591cee66d7e04b216f761a99
ducrotoy marc <marcduc99@gmail.com>
2015-07-05 17:41:59 +0200

    “exercice_1.py”

--- exercise_1.py
+++ exercise_1.py
@@ -1,10 +1,8 @@
-# AJOUTE UN
-# coding=utf-8
-#
-# Écris une fonction "ajoute_un(n)" qui prend argument un entier n et retourne n + 1.
-# Exemples : ajoute(0) retourne 1, ajoute(34893) retourne 34894, et ajoute(-1) retourne 0.
-#
-#
-# --- INSERT YOUR CODE BELOW ---
-def ajoute_un(n)
-return n + 1
+Python 3.5.0b1 (v3.5.0b1:071fefbb5e3d, May 23 2015, 18:22:54) 
+[GCC 4.2.1 (Apple Inc. build 5666) (dot 3)] on darwin
+Type "copyright", "credits" or "license()" for more information.
+>>> WARNING: The version of Tcl/Tk (8.5.9) in use may be unstable.
+Visit http://www.python.org/download/mac/tcltk/ for current information.
+
+>>> def ajoute_un(n):
+	return n + 1
