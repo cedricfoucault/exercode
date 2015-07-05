3308cf45a171f463b38e4da7f63b885e4931393b
ducrotoy marc <marcduc99@gmail.com>
2015-07-05 18:04:28 +0200

    correction

--- exercise_1.py
+++ exercise_1.py
@@ -1,8 +1,2 @@
-Python 3.5.0b1 (v3.5.0b1:071fefbb5e3d, May 23 2015, 18:22:54) 
-[GCC 4.2.1 (Apple Inc. build 5666) (dot 3)] on darwin
-Type "copyright", "credits" or "license()" for more information.
->>> WARNING: The version of Tcl/Tk (8.5.9) in use may be unstable.
-Visit http://www.python.org/download/mac/tcltk/ for current information.
-
->>> def ajoute_un(n):
+def ajoute_un(n):
 	return n + 1
